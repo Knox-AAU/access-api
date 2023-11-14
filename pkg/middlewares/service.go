f37cce84755852b22531ba3af46b26bc9e0bfe2e
PaPaReipurth <malthe@reipurth.dk>
2023-11-14 11:13:44 +0100

    [debug]

--- pkg/middlewares/service.go
+++ pkg/middlewares/service.go
@@ -1,9 +1,12 @@
 package middlewares
 
 import (
+	"fmt"
 	"net/http"
 )
 
 func Middlewares(proxy_req http.Request, proxy_res http.Response, org_req http.Request) {
 	// Implelemt stuff like logging or metrics here, idk tbh
+	fmt.Println("Debug")
+
 }
