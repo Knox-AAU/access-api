208369e36cd1e3e879eafdf2a91d40f141ef03c4
Casper Christensen <caschr21@student.aau.dk>
2023-11-17 10:38:22 +0100

    [update] separate generation of request id

--- pkg/middlewares/service.go
+++ pkg/middlewares/service.go
@@ -17,14 +17,18 @@ func LoggingMiddleware(next http.Handler) http.Handler {
 	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
 		// Save the start time, and the request id in the request context
 		startTime := time.Now()
-		requestID := time.Now().Format("20060102-15:04:05")
+		requestID := GenerateRequestID()
 		// Log the request
 		log.Printf("[%s] %s %s - Request received", requestID, r.Method, r.URL.Path)
 		// Add the request id to the response header
-		r.Header.Add("Request-ID", requestID)
+		r.Header.Add("X-Request-ID", requestID)
 		// Call the next handler in the chain
 		next.ServeHTTP(w, r)
 		// Log the response
 		log.Printf("[%s] %s %s - Response sent in %v", requestID, r.Method, r.URL.Path, time.Since(startTime))
 	})
+}
+
+func GenerateRequestID() string {
+	return time.Now().Format("20060102-15:04:05")
 }
