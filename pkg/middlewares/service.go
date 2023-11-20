497cd106e17846ea6f4fe33e7fdbe5b5d793c3a2
PaPaReipurth <malthe@reipurth.dk>
2023-11-20 09:54:32 +0100

    [ebug] testing ci/cd

--- pkg/middlewares/service.go
+++ pkg/middlewares/service.go
@@ -7,6 +7,6 @@ import (
 
 func Middlewares(proxy_req http.Request, proxy_res http.Response, org_req http.Request) {
 	// Implelemt stuff like logging or metrics here, idk tbh
-	fmt.Println("Debug")
+	fmt.Println("Everything is awsome")
 
 }
