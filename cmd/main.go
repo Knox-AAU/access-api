22a6b7667c06cb7eef596d939ef0ef9ae93567c3
Mcklmo <87199710+Mcklmo@users.noreply.github.com>
2023-12-14 14:51:12 +0100

    [fix] revert running app on port 8080

--- cmd/main.go
+++ cmd/main.go
@@ -9,7 +9,7 @@ import (
 
 func main() {
 	router := api.SetupRouter("./")
-	port := "80"
+	port := "8080"
 	log.Printf("Listening at port %s..\n", port)
 	log.Fatal(http.ListenAndServe(fmt.Sprintf(":%s", port), router))
 }
